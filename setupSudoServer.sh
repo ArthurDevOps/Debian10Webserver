471c6bb378780d9e94e956e67ad31a41c60a1370
ArthurHTW <arthur.merz@gmx.de>
2021-01-28 10:27:11 +0100

    fixed inventory in scripts

--- setupSudoServer.sh
+++ setupSudoServer.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #Run the playbook to setup sudo on the Server
-ansible-playbook -i hosts -K tasks/sudo.yml 
+#ansible-playbook -i hosts -K tasks/sudo.yml
+ansible-playbook -i inventory -K tasks/sudo.yml 
